ce590e53ee43b84054aee80f2b18bce487f2f070
Joseph Ofei <josephofei24@gmail.com>
2023-03-29 17:19:57 -0400

    array map complete

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((tutorial) => {
+    const words = tutorial.split(" ")
+    const capitalize = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    return capitalize.join(" ")
+  })
 }
